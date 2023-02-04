fbf4d839831fe9f32e520842859d96b769f28b2b
Myodong <gyehd1230@naver.com>
2023-02-04 19:22:35 +0900

    Update B2587.java

--- ProblemSolving/BaekJoon/src/step10/B2587.java
+++ ProblemSolving/BaekJoon/src/step10/B2587.java
@@ -45,23 +45,39 @@ public class B2587 {
 		System.out.print("인덱스 설정 : ");
 		int index = Integer.parseInt(bf.readLine());
 		int nNum[] = new int[index];
+		
+		// 합 선언
 		int sum=0;
 
-		
 		// 배열에 값 넣기
 		for (int i = 0; i < nNum.length; i++) {
 			nNum[i] = Integer.parseInt(bf.readLine());
-			System.out.println(Arrays.toString(nNum));
+			// 값 누적
+			sum+= nNum[i];
+		}
+
+		// 배열 오름차순 정렬
+		Arrays.sort(nNum);
+		
+		System.out.println(""); // 띄어쓰기
+		System.out.println("합계 : "+sum);
+		System.out.println("평균값 : "+sum/index);
+		
+		// 중앙값 구하기
+		int middleNumber = nNum[index/2];
+		// 인덱스가 홀수있때
+		if (index% 2 == 1) {
+			System.out.println("중앙값 : "+middleNumber);
+		
+		} else { // 인덱스가 짝수일때
+			System.out.println("중앙값 : "+(middleNumber+(nNum[((index/2)+1)]))/2 );
 		}
 		
 		
+
+
 		
 		
-		// 3. 배열에서 평균값 구하기 
-		
-		// 4. 중앙값 구하기 
-		
-		// 5. 만약 중앙값이 2개라면 두 수의 평균값 출력 
 		
 		
 		
