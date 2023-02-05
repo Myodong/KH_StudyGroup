c9cc32c2fdbd2a7502d98b5b41c3195398db7658
Myodong <gyehd1230@naver.com>
2023-02-05 18:22:53 +0900

    Update test.java

--- ProblemSolving/BaekJoon/src/test/test.java
+++ ProblemSolving/BaekJoon/src/test/test.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
 public class test {
@@ -12,12 +13,17 @@ public class test {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st=new StringTokenizer(bf.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}		
 
-	        String[] arr = new String[] {"10", "15", "30", "55"};
-
-	        Integer[] newArr = Stream.of(arr).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
-
-	        // output
-	        System.out.println(Arrays.asList(newArr));
-	}
 }
