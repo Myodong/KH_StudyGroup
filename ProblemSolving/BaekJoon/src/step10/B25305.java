6dfa78a6ebcb202ca59553fc505d01817e44f046
Myodong <gyehd1230@naver.com>
2023-02-05 17:49:27 +0900

    .

--- ProblemSolving/BaekJoon/src/step10/B25305.java
+++ ProblemSolving/BaekJoon/src/step10/B25305.java
@@ -1,8 +1,16 @@
 package step10;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
 public class B25305 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		/*
 		 * 문제 2022 연세대학교 미래캠퍼스 슬기로운 코딩생활에 N명의 학생들이 응시했다.
@@ -31,6 +39,24 @@ public class B25305 {
 		 * $1 ≤ k ≤ N
 		 * $0 ≤ x ≤ 10,000
 		 */
-
+		
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("전체인원, 상인원 =");
+		String input[] = bf.readLine().split(" ");
+		
+		
+		System.out.println("점수입력 = ");
+		String[] score = bf.readLine().split(" ");
+		
+		// Stream으로 String 배열을 Integer 배열로 변환
+//      Integer[] intScore = Stream.of(score).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
+      // Stream으로 String 배열을 int 배열로 변환
+      int[] intScore = Stream.of(score).mapToInt(Integer::parseInt).toArray();
+      // primitive Type을 Wrapper클래스로 박싱해주어야 reverseOrder 사용가능.
+      Integer[] tmp = Arrays.stream(intScore).boxed().toArray(Integer[]::new);
+        Arrays.sort(tmp, Comparator.reverseOrder());
+		System.out.println(tmp[Integer.parseInt(input[1])-1]);
 	}
 }
--- ProblemSolving/BaekJoon/src/test/test.java
+++ ProblemSolving/BaekJoon/src/test/test.java
@@ -4,45 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
 
 public class test {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		
-		// 선언
-				BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-				// 인덱스 값 입력
-				System.out.print("인덱스 설정 : ");
-				int index = Integer.parseInt(bf.readLine());
-				int nNum[] = new int[index];
-				
-				// 합 선언
-				int sum=0;
+	        String[] arr = new String[] {"10", "15", "30", "55"};
 
-				// 배열에 값 넣기
-				for (int i = 0; i < nNum.length; i++) {
-					nNum[i] = Integer.parseInt(bf.readLine());
-					// 값 누적
-					sum+= nNum[i];
-				}
+	        Integer[] newArr = Stream.of(arr).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
 
-				// 배열 오름차순 정렬
-				Arrays.sort(nNum);
-				
-				System.out.println(); // 띄어쓰기
-				System.out.println("합계 : "+sum);
-				System.out.println("평균값 : "+sum/index);
-				
-				// 중앙값 구하기
-				int middleNumber = nNum[index/2];
-				// 인덱스가 홀수있때
-				if (index% 2 == 1) {
-					System.out.println("중앙값 : "+middleNumber);
-				
-				} else { // 인덱스가 짝수일때
-					System.out.println("중앙값 : "+(middleNumber+(nNum[((index/2)+1)]))/2 );
-				}
+	        // output
+	        System.out.println(Arrays.asList(newArr));
 	}
 }
