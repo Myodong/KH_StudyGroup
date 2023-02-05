f9c3643bb4aa7f47d940da4fa7d245bf05bb3a83
Myodong <gyehd1230@naver.com>
2023-02-05 19:01:33 +0900

    Update B25305.java

--- ProblemSolving/BaekJoon/src/step10/B25305.java
+++ ProblemSolving/BaekJoon/src/step10/B25305.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
@@ -13,50 +14,66 @@ public class B25305 {
 	public static void main(String[] args) throws IOException {
 
 		/*
-		 * 문제 2022 연세대학교 미래캠퍼스 슬기로운 코딩생활에 N명의 학생들이 응시했다.
-		 * 이들 중 점수가 가장 높은 k명은 상을 받을 것이다. 이 때, 상을 받는 커트라인이 몇 점인지 구하라.
-		 * 커트라인이란 상을 받는 사람들 중 점수가 가장 가장 낮은 사람의 점수를 말한다.
+		 * 문제 2022 연세대학교 미래캠퍼스 슬기로운 코딩생활에 N명의 학생들이 응시했다. 이들 중 점수가 가장 높은 k명은 상을 받을 것이다. 이
+		 * 때, 상을 받는 커트라인이 몇 점인지 구하라. 커트라인이란 상을 받는 사람들 중 점수가 가장 가장 낮은 사람의 점수를 말한다.
 		 * 
 		 * 
 		 * 입력 
 		 * 첫째 줄에는 응시자의 수 N과 상을 받는 사람의 수 k가 공백을 사이에 두고 주어진다. 
 		 * 둘째 줄에는 각 학생의 점수 x가 공백을 사이에 두고 주어진다.
 		 * 
-		 * ex)
-		 * 5 2
-		 * 100 76 85 93 98
+		 * ex) 5 2 100 76 85 93 98
 		 * 
 		 * 
 		 * 출력 
 		 * 상을 받는 커트라인을 출력하라.
+
+		 * ex) 98
 		 * 
-		 * ex)
-		 * 98
 		 * 
-		 * 
-		 * 제한
-		 * $1 ≤ N ≤ 1,000
-		 * $1 ≤ k ≤ N
-		 * $0 ≤ x ≤ 10,000
+		 * 제한 
+		 * 1 ≤ N ≤ 1,000 
+		 * 1 ≤ k ≤ N 
+		 * 0 ≤ x ≤ 10,000
 		 */
+
 		
 		
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		System.out.println("전체인원, 상인원 =");
-		String input[] = bf.readLine().split(" ");
 		
 		
-		System.out.println("점수입력 = ");
-		String[] score = bf.readLine().split(" ");
 		
-		// Stream으로 String 배열을 Integer 배열로 변환
-//      Integer[] intScore = Stream.of(score).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
-      // Stream으로 String 배열을 int 배열로 변환
-      int[] intScore = Stream.of(score).mapToInt(Integer::parseInt).toArray();
-      // primitive Type을 Wrapper클래스로 박싱해주어야 reverseOrder 사용가능.
-      Integer[] tmp = Arrays.stream(intScore).boxed().toArray(Integer[]::new);
-        Arrays.sort(tmp, Comparator.reverseOrder());
-		System.out.println(tmp[Integer.parseInt(input[1])-1]);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/* 제출용 코드 
+		 * 
+		 * 
+		 * 
+		 * BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		 * 
+		 * System.out.println("전체인원, 상인원 ="); String input[] = bf.readLine().split(" ");
+		 * 
+		 * 
+		 * System.out.println("점수입력 = "); String[] score = bf.readLine().split(" ");
+		 * 
+		 * // Stream으로 String 배열을 int 배열로 변환 int[] intScore =
+		 * Stream.of(score).mapToInt(Integer::parseInt).toArray(); // primitive Type을
+		 * Wrapper클래스로 박싱해주어야 reverseOrder 사용가능. Integer[] tmp =
+		 * Arrays.stream(intScore).boxed().toArray(Integer[]::new); Arrays.sort(tmp,
+		 * Comparator.reverseOrder());
+		 * System.out.println(tmp[Integer.parseInt(input[1])-1]);
+		 * 
+		 */
 	}
 }
