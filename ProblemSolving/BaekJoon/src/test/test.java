7ea41c0e7b417bbb4e36080d3ae43187128a6d07
Myodong <gyehd1230@naver.com>
2023-02-13 19:47:57 +0900

    Update test.java

--- ProblemSolving/BaekJoon/src/test/test.java
+++ ProblemSolving/BaekJoon/src/test/test.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class test {
 
@@ -21,21 +22,27 @@ public class test {
 		for (int i = 0; i < input.length; i++) {
 			input[i] = Integer.parseInt(bf.readLine());
 		}
+		// 인덱스 값 오름차순 정렬
+		Arrays.sort(input);
+		
+		// 배열의 길이
+		double length = input.length;
 		
 		// 누적값 선언
-		int sum =0;
+        double sum =0;
 
 		// 인덱스 값 누적
 		for (int i = 0; i < input.length; i++) {
 			sum += input[i];
 		}
 		
-		System.out.println("---");
 		
-		System.out.println(sum);
-		System.out.println(input.length);
-		System.out.println("합1"+(sum/input.length));
-        bw.write("합2"+(sum/input.length));
+		System.out.println("---");
+		System.out.println("sum="+sum);
+		System.out.println("length="+length);
+        bw.write("1번="+Math.round(sum/length)+"\n");
+        bw.write("2번="+input[(int) (length/2)]+"\n");
+        bw.write("3번="+"\n");
 //		System.out.println((5+1+3+8+-2+2)/6);
 		bw.flush();
 		bw.close();
