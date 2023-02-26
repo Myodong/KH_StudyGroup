3f2269ee7cd0126f2c7400060ec98eb9d6e4d2d1
Myodong <gyehd1230@naver.com>
2023-02-26 17:23:01 +0900

    .

--- ProblemSolving/BaekJoon/src/step10sort/B1181.java
+++ ProblemSolving/BaekJoon/src/step10sort/B1181.java
@@ -1,9 +1,27 @@
 package step10sort;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class B1181 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		int inputN = Integer.parseInt(br.readLine());
+		
+		String arr[] = new String[inputN];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i]= br.readLine();
+		}
+		
+		
 	}
 
 }
