349a361e5385574116a8a276332e058767a9b27b
Myodong <gyehd1230@naver.com>
2023-03-28 15:55:47 +0900

    Update B10815.java

--- ProblemSolving/BaekJoon/src/step14setmap/B10815.java
+++ ProblemSolving/BaekJoon/src/step14setmap/B10815.java
@@ -16,40 +16,66 @@ import java.util.StringTokenizer;
 public class B10815 {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+		StringTokenizer st;
+
 		// 카드 개수 N 입력
 		int inputN = Integer.parseInt(br.readLine());
-        int[] cards = new int[inputN];
+		int[] cards = new int[inputN];
 
-		
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		st = new StringTokenizer(br.readLine(), " ");
 
-		// 원소 입력받기 ,map에 넣기 
+		// 상근이 보유 카드 숫자 입력받기
 		for (int i = 0; i < inputN; i++) {
-			cards[i] =Integer.parseInt(st.nextToken());
+			cards[i] = Integer.parseInt(st.nextToken());
 		}
-		
-///////////////////////////////////////////////////////////////////////////////		
-		
-		// 입력 받을 원소 갯수 인덱스 길이 M 입력 받기
-		int inputM = Integer.parseInt(br.readLine());
-		
-		Integer[] numM = new Integer[inputM];
-		
-		StringTokenizer st1 = new StringTokenizer(br.readLine()," ");
 
-		// 원소 입력 받고 BufferedWriter에 출력 문 담기
+		// 정렬하기
+		Arrays.sort(cards);
+		
+		
+		System.out.println("\n"+"------------");
+		System.out.println(Arrays.toString(cards));
+		System.out.println("------------");
+		
+
+		// 카드 개수 M 입력 받기
+		int inputM = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine());
+
 		for (int i = 0; i < inputM; i++) {
-			numM[i]=Integer.parseInt(st1.nextToken());
-			//getOrDefault 키가 없으면 디폴트 출력됨 
-			bw.write(map1.getOrDefault(numM[i],0) + " ");
+			int temp = Integer.parseInt(st.nextToken());
+			sb.append(binarySearch(cards, inputN, temp) + " ");
 		}
-		// 출력 후 스트림 닫기
-		bw.close();
+		
+        bw.write(sb.toString() + "\n");
+        bw.close();
+        br.close();
 	}
 
+	//이분탐색(이진탐색)
+	public static int binarySearch(int[] cards, int N, int search) {
+		int first = 0;
+		int last = N - 1;
+		int mid = 0;
 
+		while (first <= last) {
+			mid = (first + last) / 2; // 중간 인덱스
+
+			if (cards[mid] == search) { // 중간값과 찾으려는 수가 같은 경우
+				return 1;
+			}
+
+			if (cards[mid] < search) { // 중간값이 찾으려는 수보다 작으면, 그 이하로는 볼 필요 없음.
+				first = mid + 1;
+			} else { // 중간값이 찾으려는 수보다 크면, 그 이상으로는 볼 필요 없음.
+				last = mid - 1;
+			}
+		}
+		return 0;
+	}
 }
