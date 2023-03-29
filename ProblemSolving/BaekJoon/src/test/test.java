2a701caea873a501a5ae68561af3f3f65938823f
Myodong <gyehd1230@naver.com>
2023-03-29 18:25:17 +0900

    Update test.java

--- ProblemSolving/BaekJoon/src/test/test.java
+++ ProblemSolving/BaekJoon/src/test/test.java
@@ -1,10 +1,8 @@
 package test;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
