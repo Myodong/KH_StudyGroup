0ef03937621ad46eb2b964f65eb3bcc64d2be8cb
Myodong <gyehd1230@naver.com>
2023-03-29 17:00:54 +0900

    Update B14425_S.java

--- ProblemSolving/BaekJoon/src/step14setmap/B14425_S.java
+++ ProblemSolving/BaekJoon/src/step14setmap/B14425_S.java
@@ -11,36 +11,56 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 public class B14425_S {
+
+	static int result = 0;
+
 	public static void main(String[] args) throws Exception {
 
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-	    // 문자열 갯수 N 과 M 
-	    int inputN =Integer.parseInt(st.nextToken());
-	    int inputM =Integer.parseInt(st.nextToken());
-	    
-	    // M 단어를 Set에 넣기
-	    Set<String> setWordN = new HashSet<>();
-	    for (int i = 0; i < inputN; i++) {
-	        setWordN.add(br.readLine());
-	    }
-	    
-	    int result = 0;
-	    
-	    // SetM에서 N단어 몇개 같은지 찾기
-	    for(int i=0;i<inputM;i++) {
-	        String key = br.readLine();
-	        // contains = Set에 key값이 있는지 없는지 확인 (true,false 반환)
-	        if(setWordN.contains(key)) {
-	            result++;
-	        }
-	    }
-	    
-	    bw.write(result+"");
-	    bw.close();
-	    br.close();
-	    
+		// 문자열 갯수 N 과 M
+		int inputN = Integer.parseInt(st.nextToken());
+		int inputM = Integer.parseInt(st.nextToken());
+
+		// N배열 선언
+		String[] wordN = new String[inputN];
+
+		// N배열에 단어 넣기
+		for (int i = 0; i < inputN; i++) {
+			wordN[i] = br.readLine();
+		}
+
+		// SetM에서 N단어 몇개 같은지 찾기
+		for (int i = 0; i < inputM; i++) {
+			String search = br.readLine();
+			binarySearch(wordN, inputN, search);
+		}
+
+		bw.write(result + "");
+		bw.close();
+		br.close();
+
 	}
+
+	// 이분 탐색
+	public static int binarySearch(String[] wordN, int inputN, String search ) {
+		String mid = null;
+		int left = 0;
+		int right = inputN - 1;
+				
+		while (left<=right) {
+			mid= (left+right)/2;
+			
+			if (mid==search) {
+				return ++result;
+			}
+			
+		}
+		
+		
+		return inputN;
+	}
+
 }
