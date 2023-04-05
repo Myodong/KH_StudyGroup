5b4deed92eafa3a77d4f41c8eb6fbf2bf44cc312
Myodong <gyehd1230@naver.com>
2023-04-05 17:04:24 +0900

    Create B7785_.java

---
+++ ProblemSolving/BaekJoon/src/step14setmap/B7785_.java
@@ -0,0 +1,36 @@
+package step14setmap;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class B7785_ {
+	
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// 출입기록 수 N
+		int N = Integer.parseInt(br.readLine());
+		
+		
+		Map<String, String> logList = new HashMap<String, String>();
+		
+		// 출입 로그 입력 받기
+		for (int i = 0; i < N; i++) {
+			
+		}
+		
+		// 2차원 배열
+		
+		//정렬
+		
+		// leave 가 없는 사람은 현제 출근중이다. 
+		
+		
+	}
+}
