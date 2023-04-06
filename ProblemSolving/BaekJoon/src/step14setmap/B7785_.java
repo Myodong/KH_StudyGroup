8d25683695c72353b1bbf379fc61b2f0b729d952
Myodong <gyehd1230@naver.com>
2023-04-06 17:37:31 +0900

    Update B7785_.java

--- ProblemSolving/BaekJoon/src/step14setmap/B7785_.java
+++ ProblemSolving/BaekJoon/src/step14setmap/B7785_.java
@@ -26,8 +26,8 @@ public class B7785_ {
 	    for (int i = 0; i < N; i++) {
 	        String[] inputLog = br.readLine().split(" "); // 입력받은 값을 공백으로 분리하여 배열에 저장
 
-	        if (inputLog[1].equals("enter")) { // 출근 "enter"이면 set에 추가
-	        	setLog.add(inputLog[0]);
+	        if (inputLog[1].equals("enter")) { // 입력받은 배열 inputLog[1] 값이 출근 "enter" 같다면 
+	        	setLog.add(inputLog[0]); //출근 "enter"이면 set에 추가
 	            
 	        } else { // 퇴근 "leave"이면 set에서 제거
 	        	setLog.remove(inputLog[0]);
