8b342929a27cb596ececf728b5d72fb3261981ea
Myodong <gyehd1230@naver.com>
2023-04-06 18:07:55 +0900

    .

--- ProblemSolving/BaekJoon/src/test/test.java
+++ ProblemSolving/BaekJoon/src/test/test.java
@@ -1,75 +1,51 @@
 package test;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class test {
 
-	static class TrieNode {
-		Map<Character, TrieNode> childNode = new HashMap<>();
-		boolean terminal;
+	public static void main(String[] args) throws Exception {
 
-		TrieNode() {
-			/* no-op */
-		}
-
-		public void insert(String word) {
-			TrieNode trieNode = this;
-			for (int i = 0; i < word.length(); i++) {
-				char c = word.charAt(i);
-
-				// tmp childNode에 c없으면 추가
-				trieNode.childNode.putIfAbsent(c, new TrieNode());
-				trieNode = trieNode.childNode.get(c);
-
-				// 마지막 문자 terminal
-				if (i == word.length() - 1) {
-					trieNode.terminal = true;
-					return;
-				}
-			}
-		}
-
-		public boolean contains(String word) {
-			TrieNode trieNode = this;
-			for (int i = 0; i < word.length(); i++) {
-				char c = word.charAt(i);
-				TrieNode node = trieNode.childNode.get(c);
-
-				if (node == null) {
-					return false; // 다음 문자가 없으면 false
-				}
-				trieNode = node;
-			}
-			// 해당 단어로 종료하는 문자가 있는 경우 false
-			return trieNode.terminal;
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		TrieNode tNode = new TrieNode();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// 출입기록 수 N
+		int n = Integer.parseInt(br.readLine());
+		
+		
+		Map<String, String> logList = new HashMap<String, String>();
+		
+		// 출입 로그 입력 받기
 		for (int i = 0; i < n; i++) {
-			String in = br.readLine();
-			tNode.insert(in);
-		}
-
-		int cnt = 0;
-		for (int i = 0; i < m; i++) {
-			String str = br.readLine();
-			if (tNode.contains(str)) {
-				cnt++;
+		    String[] inputLog = br.readLine().split(" "); // 입력받은 값을 공백으로 분리하여 배열에 저장
+		    
+		    if (!logList.containsKey(inputLog[0])) { // logList(Map)에 해당 key가 없다면
+		    	logList.put(inputLog[0], inputLog[1]);  // Key , Value로 값을 저장
+		    	
+			}else {
+				logList.remove(inputLog[0]);// logList(Map)에 해당 key가 있다면 기존 키 제거(출근 후 퇴근 했기때문)
 			}
 		}
-		System.out.println(cnt);
-	}
+		
+		List<String> sortListName = new ArrayList<>(logList);
 
-}
+		// 문자열 사전 역순으로 정렬
+		Collections.sort(sortListName, Collections.reverseOrder()); 
+
+        // 퇴근안한 사람 사전 역순 출력
+		for (String name : sortListName) {
+			bw.write(name + "\n");
+	    bw.close();
+	    br.close();
+
+}}
