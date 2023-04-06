1d70520544fe150a94ffa3c99bf7e9b24c25966a
Myodong <gyehd1230@naver.com>
2023-04-06 17:05:52 +0900

    Update B7785_.java

--- ProblemSolving/BaekJoon/src/step14setmap/B7785_.java
+++ ProblemSolving/BaekJoon/src/step14setmap/B7785_.java
@@ -23,13 +23,22 @@ public class B7785_ {
 		// 출입 로그 입력 받기
 		for (int i = 0; i < N; i++) {
 		    String[] inputLog = br.readLine().split(" "); // 입력받은 값을 공백으로 분리하여 배열에 저장
-		    logList.put(inputLog[0], inputLog[1]);  // Key , Value로 값을 저장
+		    
+		    if (!logList.containsKey(inputLog[0])) { // logList(Map)에 해당 key가 없다면
+		    	
+		    	logList.put(inputLog[0], inputLog[1]);  // Key , Value로 값을 저장
+			}else {
+				logList.remove(inputLog[0]);// logList(Map)에 해당 key가 있다면 기존 키 제거(출근 후 퇴근 했기때문)
+			}
+		    
 		}
 		
-		//정렬
+		System.out.println("---"); // 경계선
 		
-		// leave 가 없는 사람은 현제 출근중이다. 
-		
+		for(String key : logList.keySet()) {
+            String value = (String) logList.get(key);
+            System.out.println(key + " : " + value);
+        }
 		
 	}
 }
