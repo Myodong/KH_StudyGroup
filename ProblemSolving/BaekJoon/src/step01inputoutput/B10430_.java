2ec0dd3ffc5aa6f4173c941152b0a25d936ec8c4
Myodong <gyehd1230@naver.com>
2023-05-11 20:45:16 +0900

    Create B10430_.java

---
+++ ProblemSolving/BaekJoon/src/step01inputoutput/B10430_.java
@@ -0,0 +1,31 @@
+package step01inputoutput;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B10430_ {
+
+	public static void main(String[] args) throws Exception {
+
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		// A B C 배열 길이 선언
+		int arr[] =new int[3];
+		
+		// 배열 집어넣기
+		for(int i=0; i<arr.length; i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		bw.write((arr[0]+arr[1])%arr[2]+"\n"+((arr[0]%arr[2])+(arr[1]%arr[2]))%arr[2]+"\n"+(arr[0]*arr[1])%arr[2]+"\n"+ ((arr[0]%arr[2])*(arr[1]%arr[2]))%arr[2]);
+		bw.close();
+		br.close();
+		
+		}
+}
