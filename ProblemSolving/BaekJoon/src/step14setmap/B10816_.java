5a9f2b39a3e4c906ce6c45a94a9e973af1295641
Myodong <gyehd1230@naver.com>
2023-05-17 18:56:07 +0900

    Update B10816_.java

--- ProblemSolving/BaekJoon/src/step14setmap/B10816_.java
+++ ProblemSolving/BaekJoon/src/step14setmap/B10816_.java
@@ -5,8 +5,6 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class B10816_ {
@@ -15,33 +13,71 @@ public class B10816_ {
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // 상근이가 가지고 있는 숫자 개수
+        StringTokenizer st;
+        // 상근이가 가지고 있는 카드 개수
         int inputN = Integer.parseInt(br.readLine());
         // 상근이가 가지고 있는 숫자
-        Integer numN[]= new Integer[inputN];
+        int numN[]= new int[inputN];
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine(), " ");
 
-        // 입력받은 N 숫자들 배열에 넣기
+        // 상근이 보유 숫자카드 입력받기
         for (int n= 0; n<numN.length;n++){
             numN[n]=Integer.parseInt(st.nextToken());
         }
+
+        // 이분탐색을 위한 정렬
+        Arrays.sort(numN);
 
         // 숫자카드 M 개수
         int inputM = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine(), " ");
 
-        // Collections.frequency 사용하기 위해 배열N을 리스트에 넣어줌
-        List<Integer> listN = Arrays.asList(numN);
-
-        // 입력받은 M숫자들을 배열에 넣으면서 상근이가 가진 listN과 비교
+        // 입력받은 M숫자들을 상근이가 가진 listN과 비교
         for (int i=0; i < inputM; i++){
-            bw.write(Collections.frequency(listN,Integer.parseInt(st.nextToken()))+" ");
+            int search = Integer.parseInt(st.nextToken());
+            int rightSearch = rightSearch(numN, search);// 중복 값중 제일 큰 인덱스
+            int leftSearch  = leftSearch(numN, search);// 중복 값중 제일 작은 인덱스
+            int reslut = leftSearch-rightSearch; // 큰값 작은값의 차이가 중복 갯수
+            bw.write(reslut+ " ");
         }
 
         bw.close();
         br.close();
     }
+    public static int leftSearch(int[] arr, int search) {
+        int left = 0;
+        int right = arr.length;
+
+        while (left < right) {
+            int mid = (left + right) / 2; // 중간 인덱스
+
+            if (arr[mid] >= search) { // 찾으려는 값이 중간 값보다 작다
+                right = mid;
+
+            } else {
+                left = mid + 1;
+            }
+        }
+        return  left;
+    }
+    public static int rightSearch(int[] arr, int search) {
+        int left = 0;
+        int right = arr.length;
+
+        while (left < right) {
+            int mid = (left + right) / 2; // 중간 인덱스
+
+            if (arr[mid] > search) { // 찾으려는 값이 중간 값보다 작다
+                right = mid;
+
+            } else {
+                left = mid + 1;
+            }
+        }
+        return  left;
+    }
+
+
 }
