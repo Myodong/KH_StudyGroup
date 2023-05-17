6c4267e70ef345067b4d1ca5d9ab0424aa77b1e0
Myodong <gyehd1230@naver.com>
2023-05-17 12:31:52 +0900

    Update B10816_.java

--- ProblemSolving/BaekJoon/src/step14setmap/B10816_.java
+++ ProblemSolving/BaekJoon/src/step14setmap/B10816_.java
@@ -1,9 +1,31 @@
 package step14setmap;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class B10816_ {
 
-    public static void main (String[] args){
+    public static void main (String[] args) throws Exception{
 
-    System.out.println("test");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        // 상근이가 가지고 있는 숫자 개수
+        int inputN = Integer.parseInt(br.readLine());
+        // 상근이가 가지고 있는 숫자
+        int numN[]= new int[inputN];
+
+
+        for (int i= 0; i<numN.length;i++){
+//            System.out.println(i);
+            numN[i]=Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(Arrays.toString(numN));
     }
 }
