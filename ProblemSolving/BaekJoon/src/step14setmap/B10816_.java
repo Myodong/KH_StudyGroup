624ed2913dd33e7a8305d3340624070838d06f6d
Myodong <gyehd1230@naver.com>
2023-05-17 12:46:28 +0900

    Update B10816_.java

--- ProblemSolving/BaekJoon/src/step14setmap/B10816_.java
+++ ProblemSolving/BaekJoon/src/step14setmap/B10816_.java
@@ -13,19 +13,36 @@ public class B10816_ {
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         // 상근이가 가지고 있는 숫자 개수
         int inputN = Integer.parseInt(br.readLine());
         // 상근이가 가지고 있는 숫자
         int numN[]= new int[inputN];
 
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        for (int i= 0; i<numN.length;i++){
-//            System.out.println(i);
-            numN[i]=Integer.parseInt(st.nextToken());
+        // 입력받은 N 숫자들 배열에 넣기
+        for (int n= 0; n<numN.length;n++){
+            numN[n]=Integer.parseInt(st.nextToken());
+        }
+
+        // 숫자카드 M 개수
+        int inputM = Integer.parseInt(br.readLine());
+        // 카드의 숫자들
+        int numM[]= new int[inputM];
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        // 숫자 넣으면서 비교하고 출력 한방에 가능하지 않나?
+        // 입력받은 M 숫자들 배열에 넣기
+        for (int m= 0; m<numM.length;m++){
+            numM[m]=Integer.parseInt(st.nextToken());
         }
 
         System.out.println(Arrays.toString(numN));
+        System.out.println(Arrays.toString(numM));
+
+        bw.close();
+        br.close();
     }
 }
