3f48d82c4de5bc19ee01226dd8d9632b3c797a39
Myodong <gyehd1230@naver.com>
2023-05-24 19:51:23 +0900

    .

--- ProblemSolving/BaekJoon/src/step01inputoutput/B2588_S.java
+++ ProblemSolving/BaekJoon/src/step01inputoutput/B2588_S.java
@@ -4,9 +4,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
-public class B2588_ {
+public class B2588_S {
 
     public static void main(String[] args) throws Exception {
 
