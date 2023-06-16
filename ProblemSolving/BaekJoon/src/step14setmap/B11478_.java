8bc433e2523353cbe15c5a2b8e6c1dbfdbe6fccd
Myodong <gyehd1230@naver.com>
2023-06-16 19:53:55 +0900

    Create B11478_.java

---
+++ ProblemSolving/BaekJoon/src/step14setmap/B11478_.java
@@ -0,0 +1,20 @@
+package step14setmap;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B11478_ {
+    public static void main(String[] arge) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        abcd
+
+                a b c d
+                ab bc cd
+                abc bcd 
+    }
+}
