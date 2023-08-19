a192997642ed3c68e487ed50e255a2e0f7e37995
Myodong <gyehd1230@naver.com>
2023-08-19 14:33:20 +0900

    .

--- ProblemSolving/BaekJoon/src/step35tree/B11725_.java
+++ ProblemSolving/BaekJoon/src/step35tree/B11725_.java
@@ -1,5 +1,4 @@
 package step35tree;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
@@ -37,15 +36,15 @@ public class B11725_ {
             System.out.println("b= "+b);
             System.out.println(Arrays.toString(arr));
 
-/*https://leejinseop.tistory.com/40*/
-            /*문제점
-            * 부모인덱스번호*2+1
-            * 보모인덱스번호*2+2
-            * (자식노드인덱스-1)/2
-            * 위 법칙은 완전 이진트리에서만 사용됨
-            * 해당 문제는 완전이진트리가 아니기 때문에 사용하면 out of index가 발생*/
 
-            /*해결방법
+            /*문제점
+             * 부모인덱스번호*2+1
+             * 보모인덱스번호*2+2
+             * (자식노드인덱스-1)/2
+             * 위 법칙은 완전 이진트리에서만 사용됨
+             * 해당 문제는 완전이진트리가 아니기 때문에 사용하면 out of index가 발생*/
+
+            /*
             root를 0번인덱스가 아닌 1번인덱스부터 시작
             * 부모노드 인덱스 = i/2 (조건 i>1)
             * 왼쪽 자식노드 인덱스 = i*2 (조건 i*2 <= n)
@@ -56,21 +55,17 @@ public class B11725_ {
                 System.out.println("a,b 둘중 하나라도 1이 맞다면");
                 if (a==1){                                           // a가 1일때
                     arr[1]=a;                                        // 1인덱스에 a원소 1를 넣는다
-//                    if (arr[1*2]==null && arr[1*2] <= n){            // 1인덱스 왼쪽 자식노드가 null 이라면
-                    if (arr[1*2]==null){            // 1인덱스 왼쪽 자식노드가 null 이라면
+                    if (arr[1*2]==null){                             // 1인덱스 왼쪽 자식노드가 null 이라면
                         arr[1*2]=b;                                  // 왼쪽 자식노드에 b 값을 넣는다
-//                    }else if (arr[1*2+1]==null && arr[1*2+1] <= n){  // 1인덱스 왼쪽 자식노드가 null이 아니면
-                    }else if (arr[1*2+1]==null){  // 1인덱스 왼쪽 자식노드가 null이 아니면
+                    }else if (arr[1*2+1]==null){                     // 1인덱스 왼쪽 자식노드가 null이 아니면
                         arr[1*2+1]=b;                                // 오른쪽 자식노드에 b값을 넣는다
                     }
 
                 } else if (b==1) {                                  // b가 1일때
                     arr[1]=b;                                       // 1인덱스에 b원소 1를 넣는다
-//                    if (arr[1*2]==null && arr[1*2] <= n){           // 1인덱스 왼쪽 자식노드가 null 이라면
-                    if (arr[1*2]==null ){           // 1인덱스 왼쪽 자식노드가 null 이라면
-                            arr[1*2]=a;                                 // 왼쪽 자식노드에 a 값을 넣는다
-//                    }else if (arr[1*2+1]==null && arr[1*2+1] <= n){ // 1인덱스 왼쪽 자식노드가 null이 아니면
-                    }else if (arr[1*2+1]==null){ // 1인덱스 왼쪽 자식노드가 null이 아니면
+                    if (arr[1*2]==null ){                           // 1인덱스 왼쪽 자식노드가 null 이라면
+                        arr[1*2]=a;                             // 왼쪽 자식노드에 a 값을 넣는다
+                    }else if (arr[1*2+1]==null){                    // 1인덱스 왼쪽 자식노드가 null이 아니면
                         arr[1*2+1]=a;                               // 오른쪽 자식노드에 a값을 넣는다
                     }
                 }
@@ -79,21 +74,18 @@ public class B11725_ {
                 if (Arrays.asList(arr).contains(a)){                            //a가 배열에 이미 있다면
                     indxNum = Arrays.asList(arr).indexOf(a);                    //배열에 있는 a 인덱스 구하기
                     System.out.println("a인덱스번호=" +indxNum);
-//                    if (arr[indxNum*2]==null && arr[indxNum*2] <= n){           // 1인덱스 왼쪽 자식노드가 null 이라면
-                    if (arr[indxNum*2]==null) {           // 1인덱스 왼쪽 자식노드가 null 이라면
-                            arr[indxNum*2]=b;                                       // 왼쪽 자식노드에 a 값을 넣는다
-//                    }else if(arr[indxNum*2+1]==null && arr[indxNum*2+1] <= n){  // 1인덱스 왼쪽 자식노드가 null이 아니면
-                    }else if(arr[indxNum*2+1]==null ){  // 1인덱스 왼쪽 자식노드가 null이 아니면
+                    if (arr[indxNum*2]==null) {                                 // 1인덱스 왼쪽 자식노드가 null 이라면
+                        arr[indxNum*2]=b;                                   // 왼쪽 자식노드에 a 값을 넣는다
+                    }else if(arr[indxNum*2+1]==null ){                          // 1인덱스 왼쪽 자식노드가 null이 아니면
                         arr[indxNum*2+1]=b;                                     // 오른쪽 자식노드에 a값을 넣는다
                     }
+
                 } else if (Arrays.asList(arr).contains(b)) {                    //b가 배열에 이미 있다면
                     indxNum = Arrays.asList(arr).indexOf(b);                    //배열에 있는 b 인덱스 구하기
                     System.out.println("b인덱스번호=" +indxNum);
-//                    if (arr[indxNum*2]==null && arr[indxNum*2] <= n){           // 1인덱스 왼쪽 자식노드가 null 이라면
-                    if (arr[indxNum*2]==null ){           // 1인덱스 왼쪽 자식노드가 null 이라면
+                    if (arr[indxNum*2]==null ){                                 // 1인덱스 왼쪽 자식노드가 null 이라면
                         arr[indxNum*2]=a;                                       // 왼쪽 자식노드에 a 값을 넣는다
-//                    }else if(arr[indxNum*2+1]==null && arr[indxNum*2+1] <= n) { // 1인덱스 왼쪽 자식노드가 null이 아니면
-                    }else if(arr[indxNum*2+1]==null) { // 1인덱스 왼쪽 자식노드가 null이 아니면
+                    }else if(arr[indxNum*2+1]==null) {                          // 1인덱스 왼쪽 자식노드가 null이 아니면
                         arr[indxNum*2+1]=a;                                     // 오른쪽 자식노드에 a값을 넣는다
                     }
                 }
@@ -111,3 +103,4 @@ public class B11725_ {
 
     }
 }
+
--- ProblemSolving/BaekJoon/src/test/Test.java
+++ ProblemSolving/BaekJoon/src/test/Test.java
@@ -1,46 +1,68 @@
 package test;
 
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Test {
 
 
-	    public static void main(String[] args) {
-	    	for (int i = 0; i < 4; i++) {
-	            for (int j = 0; j < 7; j++) {
-	                if (i == 0) {
-	                    if (j == 0 || j == 6) {
-	                        System.out.print("\\");
-	                    } else {
-	                        System.out.print(" ");
-	                    }
-	                } else if (i == 1) {
-	                    if (j == 0 || j == 6) {
-	                        System.out.print("'");
-	                    } else {
-	                        System.out.print(" ");
-	                    }
-	                } else if (i == 2) {
-	                    if (j == 0) {
-	                        System.out.print("(");
-	                    } else if (j == 6) {
-	                        System.out.print(")");
-	                    } else {
-	                        System.out.print(" ");
-	                    }
-	                } else if (i == 3) {
-	                    if (j == 2) {
-	                        System.out.print("/");
-	                    } else if (j == 4) {
-	                        System.out.print("|");
-	                    } else {
-	                        System.out.print(" ");
-	                    }
-	                }
-	            }
-	            System.out.println();
-	        }
-	    }
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		// 1. 첫째 줄에서 노드의 개수 N을 입력 받기
+		int n = Integer.parseInt(br.readLine());
+
+		// 트리 구조를 표현하기 위한 그래프 구현
+		ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			tree.add(new ArrayList<>());
+		}
+
+		// 2. 노드 연결 정보 입력 받아 그래프 구성
+		for (int i = 0; i < n - 1; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int n1 = Integer.parseInt(st.nextToken()) - 1; // 노드 번호는 0부터 시작하므로 1 빼기
+			int n2 = Integer.parseInt(st.nextToken()) - 1; // 노드 번호는 0부터 시작하므로 1 빼기
+			tree.get(n1).add(n2); // 노드 n1과 n2를 연결
+			tree.get(n2).add(n1); // 노드 n2와 n1을 연결 (양방향)
+		}
+
+		boolean[] visited = new boolean[n]; // 방문 여부 확인용 배열
+		int[] parentNode = new int[n];      // 부모 노드 저장 배열
+
+		// 3. BFS를 통해 부모 노드 찾기 레벨 0부터 끝까지 차례대로 순회
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(0); // 시작 노드는 0 (루트 노드)
+		visited[0] = true; // 시작 노드 방문 표시
+		while (!queue.isEmpty()) {
+			int v = queue.remove(); // 큐에서 노드를 하나 꺼내옴
+			for (int node : tree.get(v)) { // 현재 노드의 연결된 모든 노드에 대해서
+				if (!visited[node]) {
+					visited[node] = true; // 방문 표시
+					queue.add(node); // 큐에 추가하여 다음에 방문할 노드로 지정
+					parentNode[node] = v; // 부모 노드를 현재 노드로 설정
+				}
+			}
+		}
+
+		// 4. 루트 노드를 제외한 모든 노드의 부모 노드 출력
+		for (int i = 1; i < n; i++) {
+			bw.write(parentNode[i] + 1+"\n"); // 노드 번호는 0부터 시작하므로 1 더하기
+		}
+
+		bw.close();
+		br.close();
+	}
 
 
 
