a5d63052a6803f4cb97639953f94b0a3ab5e19cb
Myodong <gyehd1230@naver.com>
2023-09-16 12:42:36 +0900

    .

--- ProblemSolving/BaekJoon/src/test/Test2.java
+++ ProblemSolving/BaekJoon/src/test/Test2.java
@@ -6,20 +6,15 @@ public class Test2 {
 
 	public static void main(String[] arge) {
 
-		// 배열 선언 및 초기화
-		int arr[] = { 2, 5, 1, 3, 0, 4 };
+		String description = null;
 
-		for (int i = 0; i < arr.length; i++) {
-			int temp = arr[i];
-			int prev = i - 1;
+		if (!description.equals(null)){
+			System.out.println("널아님"+description);
 
-			while (prev >= 0 && arr[prev] > temp) {
-				arr[prev + 1] = arr[prev];
-				prev--;
-			}
-			arr[prev + 1] = temp;
+		}else {
+			System.out.println("널"+description);
+
 		}
-		System.out.println(Arrays.toString(arr));
 
 	}
 
