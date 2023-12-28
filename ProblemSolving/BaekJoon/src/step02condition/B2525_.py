94bb2bd8d9cc7fe9b9db1bf9f3199be973c651ec
Hyodong Kim <gyehd1230@naver.com>
2023-12-28 14:19:43 +0900

    Create B2525_.py

---
+++ ProblemSolving/BaekJoon/src/step02condition/B2525_.py
@@ -0,0 +1,17 @@
+inputH,inputM=input().split()
+inputA = input()
+H = int(inputH)
+M = int(inputM)
+A = int(inputA)
+
+H += int(A//60)  # 시 합산
+M += int(A%60)  # 분 합산
+
+if M >= 60:
+    H += 1
+    M -= 60
+if H >= 24:
+    H -= 24
+
+print(H, M)
+
